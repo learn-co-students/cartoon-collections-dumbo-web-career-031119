0140786bf6933c3a28af4b46b70bc8cd63aae16f
edwin ramos <edwinramos269@gmail.com>
2019-02-06 21:30:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,22 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(array)
-  array.each do |item|
-    item.capitalize!
-    array.push("#{item}!")
-    array.unshift
+  array.map! do |item|
+   item.capitalize!+"!"
   end 
   return array 
-end 
+end
+
+def long_planeteer_calls(array)
+ if array.find_index {|item|item.size>4}==nil
+   return false
+ else true
+ end 
+end
+
+def find_the_cheese(array)
+  #array = ["banana", "cheddar", "sock"]
+  cheese=["cheddar","gouda","camembert"]
+  array.find {|item| cheese.include?(item)}
+end
   
